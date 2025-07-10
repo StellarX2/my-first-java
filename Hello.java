843334f7f73a6ca839698afbee96dfd75e2d7bf7
StellarX2 <skobelkin2017@yandex.ru>
2025-07-10 23:54:20 +0300

    Revert "Update Hello.java"
    This reverts commit ab9a271151d30c756d2ba8ede2d4984c6a356f9d.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("ъъъъъъъъъъъъъъъъъъъъъъъъ");
+        System.out.println("Привет, Git и Java!");
     }
 }
